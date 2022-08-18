df34d473ffa6dbe16d7cb7e5e83761bf6a2b4d1e
unknown <norman.mgu@gmail.com>
2022-08-18 06:45:57 -0700

    added logout functionality and also css to login page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose      = require("mongoose");
 const ejs           = require("ejs");
 const User          = require("./models/User");
 const passport      = require("passport");
+const flash         = require("connect-flash");
+const methodOverride= require("method-override");
 const app           = express();
 
 // Establish connection to MongoDB
@@ -28,7 +30,8 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 // Passport.js Setup
 app.use(passport.initialize());
@@ -63,6 +66,13 @@ app.post('/users/login', passport.authenticate('local', {
 	successRedirect: '/',
 	failureRedirect: '/login?error=true'
 }));
+
+app.delete("/users/logout", (req, res, next) =>{
+    req.logout(function(err) {
+        if (err) { return next(err)}
+        res.redirect("/");
+    });
+})
 
 app.listen(3000, () =>{
     console.log("app listening on port 3000")
